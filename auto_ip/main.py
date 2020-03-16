216d6c30d0c1bc42ad0feeb306a1dc8e7550f90a
axbg <bisagalexstefan@gmail.com>
2020-03-16 20:38:54 +0200

    Implemented execution with headless chrome

--- auto_ip/main.py
+++ auto_ip/main.py
@@ -3,11 +3,18 @@ import os
 from dotenv import load_dotenv
 from requests import get
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 import providers
 
 load_dotenv(".env")
-driver = webdriver.Chrome()
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("window-size=1920,1080")
+
+driver = webdriver.Chrome("./chromedriver.exe", options=chrome_options)
+
 ip = get('https://api.ipify.org').text
 
 index = 0
@@ -33,3 +40,4 @@ try:
         index += 1
 finally:
     driver.close()
+    print("Execution finished")
